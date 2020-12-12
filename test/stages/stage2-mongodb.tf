c3cf2e047d50884536d9342306cd5a347b16fd1d
Sean Sundberg <seansund@us.ibm.com>
2020-12-12 00:43:31 -0600

    Fixes test logic

---
+++ test/stages/stage2-mongodb.tf
@@ -0,0 +1,10 @@
+module "dev_tools_mongodb" {
+  source = "./module"
+
+  resource_group_name = var.resource_group_name
+  resource_location = var.region
+  cluster_id = module.dev_cluster.id
+  namespaces = [module.dev_capture_tools_state.namespace]
+  namespace_count = 1
+  name_prefix = var.cluster_name
+}
