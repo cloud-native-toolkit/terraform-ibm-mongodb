ef52ce9014ffbf184eacff3e9c8320c4ac997b71
Sean Sundberg <seansund@us.ibm.com>
2020-12-12 10:45:43 -0600

    wip

--- main.tf
+++ main.tf
@@ -13,13 +13,25 @@ locals {
   resource_location = var.resource_location
 }
 
+data "ibm_resource_instance" "kp_instance" {
+  name = var.key-protect-name
+  region = var.key-protect-region
+}
+
+data "ibm_kp_key" "mondodb-key" {
+  key_protect_id = data.ibm_resource_instance.kp_instance.guid
+  key_name = var.key-protect-key
+}
+
 resource "ibm_resource_instance" "mongodb_instance" {
-  name              = local.name
-  service           = local.service
-  plan              = var.plan
-  location          = local.resource_location
-  resource_group_id = data.ibm_resource_group.tools_resource_group.id
-  tags              = var.tags
+  name                 = local.name
+  service              = local.service
+  plan                 = var.plan
+  location             = local.resource_location
+  resource_group_id    = data.ibm_resource_group.tools_resource_group.id
+  key_protect_instance = data.ibm_resource_instance.kp_instance.guid
+  key_protect_key      = data.ibm_kp_key.mongodb-key.id
+  tags                 = var.tags
 
   timeouts {
     create = "30m"
