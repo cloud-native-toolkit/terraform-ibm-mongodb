27968d51a2be4aae75fe6ec9cb2de3b50c1bd117
Sean Sundberg <seansund@us.ibm.com>
2020-12-14 10:00:59 -0600

    Adds logging for binding

--- main.tf
+++ main.tf
@@ -50,14 +50,21 @@ resource "ibm_resource_key" "mongodb_key" {
   }
 }
 
+resource "null_resource" "print_config" {
+  provisioner "local-exec" {
+    command = "Binding to cluster: ${var.cluster_id}, service id: ${data.ibm_resource_instance.mongodb_instance.id}"
+  }
+}
+
 resource "ibm_container_bind_service" "mongodb_service_binding" {
   count = var.namespace_count
+  depends_on = ["null_resource.print_config"]
 
   cluster_name_id       = var.cluster_id
   service_instance_id   = data.ibm_resource_instance.mongodb_instance.id
   namespace_id          = var.namespaces[count.index]
   resource_group_id     = data.ibm_resource_group.tools_resource_group.id
-  key                   = ibm_resource_key.mongodb_key.name
+  key                   = ibm_resource_key.mongodb_key.id
 
   // The provider (v16.1) is incorrectly registering that these values change each time,
   // this may be removed in the future if this is fixed.
